ffc65d6e0a6921e9d7401822783c9c67b8a5e60d
nbbeom <5877177a@naver.com>
2021-02-27 23:44:43 +0900

    movie api 에서 이미지와 정보를 가져와 출력

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,23 +1,54 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import {API_URL} from '../../Config'
-import {API_KEY} from '../../Config'
+import {API_KEY ,IMAGE_BASE_URL} from '../../Config'
+import MainImage from './Sections/MainImage'
+import GridCard from '../commons/GridCard'
+import { Row } from 'antd';
+
 function LandingPage() {
-    const [Movies, setMovies] = useState([])
-    useEffect(()=>{
-        const endpoint = '${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1';
+    const [Movies, setMovies] = useState([]);
+    const [MainMovieImage, setMainMovieImage] = useState(null);
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    
         fetch(endpoint)
-        .then(response => response.json())
-        .then(response=>{
-            setMovies([response.results])
-        })
-    })
+        .then((response) => response.json())
+        .then((response) => {
+            setMovies(...[response.results]);
+            setMainMovieImage(response.results[0]);
+        });
+    }, []);
     return (
         <div style={{width:'100%',margin:'0%'}}>
             {/*main image*/}
+            {MainMovieImage && (
+        <MainImage
+        image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+        title={MainMovieImage.original_title}
+        text={MainMovieImage.overview}
+        />
+    )}
             <div style={{width:'85%',margin:'1rem auto'}}>
                 <h2>Movies by lastest</h2>
                 <hr/>
+                
+                <Row gutter={[16, 16]}>
+                {Movies &&
+                    Movies.map((movie, index) => (
+                    <React.Fragment key={index}>
+                        <GridCard
+                        image={
+                            movie.poster_path
+                            ? `${IMAGE_BASE_URL}w500${movie.poster_path}`
+                            : null
+                        }
+                        movieId={movie.id}
+                        movieName={movie.original_title}
+                        />
+                    </React.Fragment>
+                    ))}
+                </Row>
             </div>
 
         <div style={{display:'flex', justifyContent:'center'}}>
